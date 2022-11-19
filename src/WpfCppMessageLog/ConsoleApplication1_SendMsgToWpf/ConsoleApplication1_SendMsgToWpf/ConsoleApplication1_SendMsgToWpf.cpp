cd810934547bd265d86a94e0802acc66841fd847
yp124202435 <17792351859@189.cn>
2022-11-20 00:30:02 +0800

    添加跨进程 发送线程消息的 demo

---
+++ src/WpfCppMessageLog/ConsoleApplication1_SendMsgToWpf/ConsoleApplication1_SendMsgToWpf/ConsoleApplication1_SendMsgToWpf.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define WM_HOTKEY__1 65536
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	MSG msg = { 0 };
+
+	//while (GetMessage(&msg, NULL, 0, 0) != 0)
+	//{
+	//	if (msg.message == WM_HOTKEY__1)
+	//	{
+	//		//(L"WM_HOTKEY received\n");
+	//	}
+	//}
+
+	//手动改变这个 线程id，并向wpf进程的指定线程发送 消息
+	int thdId = 21244;//这个线程id 是osid，不是  托管线程中的 id，如5，它只是 托管程序中 进程自己编号 的id 在 os层面不识别它的
+
+	for (int i = 0; i < 10; i++)
+	{
+		PostThreadMessageW(thdId, 65536, 0, 0);
+		Sleep(100);
+	}
+
+	PostThreadMessageW(thdId, 65535, 0, 0);
+
+	return 0;
+}
---
+++ src/WpfCppMessageLog/WpfCppMessageLog/App.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCppMessageLog
+{
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct tagMSG
+    {
+        /// HWND->HWND__*
+        public System.IntPtr hwnd;
+
+        /// UINT->int
+        public int message;
+
+        /// WPARAM->UINT_PTR->int
+        public int wParam;
+
+        /// LPARAM->LONG_PTR->int
+        public int lParam;
+
+        /// DWORD->int
+        public int time;
+
+        /// POINT->tagPOINT
+        public Point pt;
+    }
+
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private const string strDateFormat = "yyyy-MM-dd HH-mm-ss fff";
+
+        public static string LogFilePath { get; set; } = string.Empty;
+
+        private static StreamWriter _sw;
+
+        public static int LogThreadId { get; private set; } = 0;
+
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "GetMessageW")]
+        [return: System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool GetMessageW(ref tagMSG lpMsg, System.IntPtr hWnd, int wMsgFilterMin, int wMsgFilterMax);
+
+        [DllImport("Kernel32", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
+        public static extern Int32 GetCurrentWin32ThreadId();
+
+        public App()
+        {
+            LogFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{DateTime.Now.ToString(strDateFormat)}.log");
+
+            Thread thd = new Thread(() =>
+            {
+                _sw = new StreamWriter(LogFilePath, true, Encoding.Unicode);
+                //LogThreadId = Thread.GetCurrentProcessorId();
+                //Trace.WriteLine($"LogThreadId = {LogThreadId}");
+
+                //Thread thd2 = Thread.CurrentThread;
+                //int id2 = thd2.ManagedThreadId;
+
+                //Trace.WriteLine($"id2 = {id2}");
+
+                LogThreadId = GetCurrentWin32ThreadId();
+                Trace.WriteLine(LogThreadId);
+
+                tagMSG msg = new tagMSG();
+                IntPtr hWnd = IntPtr.Zero;
+                while (GetMessageW(ref msg, hWnd, 0, 0))
+                {
+                    switch (msg.message)
+                    {
+                        case 65535:
+                            //_sw.WriteLine($"{DateTime.Now.ToString(strDateFormat)}");
+                            _sw.Flush();
+                            _sw.Close();
+                            return;
+                            break;
+                        case 65536:
+                            _sw.WriteLine($"{DateTime.Now.ToString(strDateFormat)}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            });
+
+            thd.Start();
+        }
+
+    }
+}
